3148b0177786ff45597f7fbb0be12dc631bc1618
Wekkel <wekkel_e@hotmail.com>
2019-04-06 17:57:52 +0200

    Add project files.

---
+++ BasicPDFCompare/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicPDFCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
