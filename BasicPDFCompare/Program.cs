87bdeccb6c471dba9b8dfbe7be7778777ef58ef4
Wekkel <wekkel_e@hotmail.com>
2019-04-06 19:46:35 +0200

    basic console menu structure

--- BasicPDFCompare/Program.cs
+++ BasicPDFCompare/Program.cs
@@ -9,6 +9,42 @@ namespace BasicPDFCompare
     {
         static void Main(string[] args)
         {
+
+            bool playagain = true;
+            
+            {
+                if (args == null)
+                {
+                    Console.WriteLine("you did not provide arguments to the executable");
+                    Console.WriteLine("please provide 3 arguments ([source_file1] [source2_file] [destination_file]");
+                    Console.ReadLine();
+                }
+                else if (args.Length == 1)
+                {
+                    Console.WriteLine("you only provided 1 argument to the executable");
+                    Console.WriteLine("please provide 3 arguments ([source_file1] [source2_file] [destination_file]");
+                    Console.ReadLine();
+                }
+                else if (args.Length == 2)
+                {
+                    Console.WriteLine("you only provided 2 arguments to the executable");
+                    Console.WriteLine("please provide 3 arguments ([source_file1] [source2_file] [destination_file]");
+                    Console.ReadLine();
+                }
+                else if (args.Length == 3)
+                {
+                    Console.WriteLine("you provided 3 arguments to the executable");
+                    Console.WriteLine("Thanks. The rest of the program is still under construction");
+                    Console.ReadLine();
+                }
+                else if (args.Length > 3)
+                {
+                    Console.WriteLine("you provided more than 3 arguments to the executable");
+                    Console.WriteLine("please provide 3 arguments: [source_file1] [source2_file] [destination_file]");
+                    Console.ReadLine();
+                }
+            }
+            
         }
     }
 }
